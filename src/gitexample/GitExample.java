3a3984000d394661cb8d058ff8e7b382cbedddc3
RobChung <chungrob@sheridancollege.ca>
2020-01-23 09:30:03 -0500

    2nd commit

--- src/gitexample/GitExample.java
+++ src/gitexample/GitExample.java
@@ -17,7 +17,8 @@ public class GitExample {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("This is a modification of the file.");
     }
 
 }
