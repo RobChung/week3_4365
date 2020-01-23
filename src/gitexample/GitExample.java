14b15a4ac2104a246e70f4ca9d49a50cf7850ace
RobChung <chungrob@sheridancollege.ca>
2020-01-23 09:27:39 -0500

    First commit of the project

---
+++ src/gitexample/GitExample.java
@@ -0,0 +1,23 @@
+/*
+    To review the concepts from last week; GitRepo
+    
+    Description:
+    [program description in your own words]
+*/
+
+package gitexample;
+
+/**
+ *
+ * @author Vanhae
+ */
+public class GitExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
